661031ff001ef49c6d4486074e38a9634c060840
Raymond LeClair <raymond.leclair@gmail.com>
2025-01-30 12:15:09 -0500

    Add ArangoDB password

--- cell-kn-demos/sh/setup.sh
+++ cell-kn-demos/sh/setup.sh
@@ -1,4 +1,5 @@
 export ARANGO_DB_HOME="/Users/raymondleclair/Projects/NIH-NLM/Cell-KN/springbok-cell-kn-demos/cell-kn-demos/data/arangodb"
+export ARANGO_DB_PASSWORD="7mtgagy6hFx46ASX"
 runoak set-apikey -e bioportal $BIOPORTAL_API_KEY
 runoak set-apikey -e ncbi-email raymond.leclair@gmail.com
 runoak set-apikey -e ncbi-key $NCBI_API_KEY
